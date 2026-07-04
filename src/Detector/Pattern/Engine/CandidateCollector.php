sinefineinfinitum/ponymator

feat: add detection engine

src/Detector/Pattern/Engine/CandidateCollector.php
@@ -1,0 +1,88 @@
+<?php declare(strict_types=1);
+
+namespace SineFine\Ponymator\Detector\Pattern\Engine;
+
+use PDO;
+use SineFine\Ponymator\Detector\Pattern\Catalog\PatternInterface;
+use SineFine\Ponymator\Detector\Pattern\Model\PatternMatch;
+use SineFine\Ponymator\Detector\Pattern\Model\PatternParticipant;
+
+/**
+ * Collects candidate pattern matches by executing SQL from PatternInterface::candidateSql().
+ *
+ * TRUST BOUNDARY: The SQL executed here originates exclusively from built-in
+ * PatternInterface implementations registered in PatternRegistry. These are
+ * compile-time constants, not user-supplied input. If the pattern catalog is
+ * extended to load patterns from external sources (plugins, config files, etc.),
+ * the SQL must be validated before execution (e.g., read-only assertion, EXPLAIN
+ * pre-check, or allowlist of query structures).
+ */
+final class CandidateCollector
+{
+    public function __construct(
+        private PDO              $pdo,
+        private PatternInterface $pattern,
+    ) {
+    }
+
+    /**
+     * @return PatternMatch[]
+     */
+    public function collect(): array
+    {
+        $sql = $this->pattern->candidateSql();
+
+        if (empty($sql)) {
+            return [];
+        }
+
+        return $this->collectFromSql($sql);
+    }
+
+    /**
+     * @return PatternMatch[]
+     */
+    private function collectFromSql(string $sql): array
+    {
+        $stmt = $this->pdo->query($sql);
+        if ($stmt === false) {
+            return [];
+        }
+        /** @var list<array<string, mixed>> $rows */
+        $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        /** @var array<int, list<array{entityId: int, role: string}>> $grouped */
+        $grouped = [];
+        foreach ($rows as $row) {
+            $matchId = isset($row['match_id']) ? (int) $row['match_id'] : 0;
+            $entityId = isset($row['entity_id']) ? (int) $row['entity_id'] : 0;
+            if ($matchId === 0 || $entityId === 0) {
+                continue;
+            }
+
+            $role = isset($row['role']) && is_string($row['role']) ? $row['role'] : '';
+            if ($role === '') {
+                continue;
+            }
+            $dedupeKey = $entityId . "\0" . $role;
+            $grouped[$matchId][$dedupeKey] = ['entityId' => $entityId, 'role' => $role];
+        }
+
+        $matches = [];
+        foreach ($grouped as $participants) {
+            $patternParticipants = [];
+            foreach ($participants as $p) {
+                $patternParticipants[] = new PatternParticipant(role: $p['role'], entityId: $p['entityId']);
+            }
+
+            if ($patternParticipants !== []) {
+                $matches[] = new PatternMatch(
+                    pattern: $this->pattern,
+                    participants: $patternParticipants,
+                );
+            }
+        }
+
+        return $matches;
+    }
+}

src/Detector/Pattern/Engine/Engine.php
@@ -1,0 +1,45 @@
+<?php declare(strict_types=1);
+
+namespace SineFine\Ponymator\Detector\Pattern\Engine;
+
+use PDO;
+use SineFine\Ponymator\Detector\Pattern\Model\PatternResult;
+use Throwable;
+
+final class Engine
+{
+    public function __construct(
+        private PatternRegistry $registry,
+        private PDO $pdo,
+        private ?PDO $readOnlyPdo = null,
+    ) {
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function run(): PatternResult
+    {
+        $allMatches = [];
+        $errors = [];
+        $detectionPdo = $this->readOnlyPdo ?? $this->pdo;
+
+        foreach ($this->registry->all() as $pattern) {
+            try {
+                $collector = new CandidateCollector($detectionPdo, $pattern);
+                $matches = $collector->collect();
+                $allMatches = [...$allMatches, ...$matches];
+            } catch (\PDOException $e) {
+                $errors[] = "Pattern '{$pattern->name()}': " . $e->getMessage();
+            }
+        }
+
+        $persister = new MatchPersister($this->pdo);
+        $persister->record($allMatches);
+
+        return new PatternResult(
+            matches: $allMatches,
+            errors: $errors,
+        );
+    }
+}

src/Detector/Pattern/Engine/MatchPersister.php
@@ -1,0 +1,60 @@
+<?php declare(strict_types=1);
+
+namespace SineFine\Ponymator\Detector\Pattern\Engine;
+
+use PDO;
+use SineFine\Ponymator\Detector\Pattern\Model\PatternMatch;
+use Throwable;
+
+final class MatchPersister
+{
+    public function __construct(
+        private PDO $pdo,
+    ) {
+    }
+
+    /**
+     * @param  PatternMatch[] $matches
+     * @throws Throwable
+     */
+    public function record(array $matches): void
+    {
+        $this->pdo->beginTransaction();
+        try {
+            $this->pdo->exec('DELETE FROM pattern_participants');
+            $this->pdo->exec('DELETE FROM pattern_matches');
+
+            $insertMatch = $this->pdo->prepare(
+                'INSERT INTO pattern_matches (pattern_name) VALUES (:name)'
+            );
+            $insertPart = $this->pdo->prepare(
+                'INSERT INTO pattern_participants (match_id, entity_id, role) VALUES (:match_id, :entity_id, :role)'
+            );
+
+            foreach ($matches as $match) {
+                $insertMatch->execute(
+                    [
+                    'name' => $match->pattern->name(),
+                    ]
+                );
+
+                $matchId = (int) $this->pdo->lastInsertId();
+
+                foreach ($match->participants as $participant) {
+                    $insertPart->execute(
+                        [
+                        'match_id' => $matchId,
+                        'entity_id' => $participant->entityId,
+                        'role' => $participant->role,
+                        ]
+                    );
+                }
+            }
+
+            $this->pdo->commit();
+        } catch (Throwable $e) {
+            $this->pdo->rollBack();
+            throw $e;
+        }
+    }
+}

src/Detector/Pattern/Engine/PatternRegistry.php
@@ -1,0 +1,39 @@
+<?php declare(strict_types=1);
+
+namespace SineFine\Ponymator\Detector\Pattern\Engine;
+
+use SineFine\Ponymator\Detector\Pattern\Catalog\PatternInterface;
+
+final class PatternRegistry
+{
+    /** @var PatternInterface[] */
+    private array $patterns = [];
+
+    /**
+     * @param PatternInterface[] $patterns
+     */
+    public function __construct(array $patterns = [])
+    {
+        foreach ($patterns as $pattern) {
+            $this->register($pattern);
+        }
+    }
+
+    public function register(PatternInterface $pattern): void
+    {
+        $this->patterns[$pattern->name()] = $pattern;
+    }
+
+    public function get(string $name): ?PatternInterface
+    {
+        return $this->patterns[$name] ?? null;
+    }
+
+    /**
+     * @return PatternInterface[]
+     */
+    public function all(): array
+    {
+        return array_values($this->patterns);
+    }
+}

tests/Unit/Detector/Pattern/Pipeline/MatchPersisterTest.php
@@ -1,0 +1,103 @@
+<?php declare(strict_types=1);
+
+namespace SineFine\Ponymator\Tests\Unit\Detector\Pattern\Pipeline;
+
+use PDO;
+use PHPUnit\Framework\TestCase;
+use SineFine\Ponymator\Detector\Pattern\Engine\MatchPersister;
+use SineFine\Ponymator\Detector\Pattern\Model\PatternMatch;
+use SineFine\Ponymator\Detector\Pattern\Model\PatternParticipant;
+use SineFine\Ponymator\Graph\Experimental\GraphCommand;
+use SineFine\Ponymator\Graph\Experimental\GraphQuery;
+use SineFine\Ponymator\Graph\Experimental\Schema;
+use SineFine\Ponymator\Tests\Unit\Detector\Pattern\Stub\PatternInterfaceStub;
+
+final class MatchPersisterTest extends TestCase
+{
+    private PDO $pdo;
+    private GraphQuery $query;
+    private GraphCommand $command;
+
+    protected function setUp(): void
+    {
+        $this->pdo = new PDO('sqlite::memory:');
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        Schema::create($this->pdo);
+        $this->command = new GraphCommand($this->pdo);
+        $this->query = new GraphQuery($this->pdo);
+    }
+
+    public function testRecordStoresMatches(): void
+    {
+        $entityId = $this->command->insertEntity(
+            'App\\TargetInterface', 'TargetInterface', 'interface', null, null, null, [],
+        );
+
+        $pattern = new PatternInterfaceStub('adapter', ['target']);
+        $match = new PatternMatch(
+            pattern: $pattern,
+            participants: [
+                new PatternParticipant(role: 'target', entityId: $entityId),
+            ],
+        );
+
+        $persister = new MatchPersister($this->pdo);
+        $persister->record([$match]);
+
+        $matches = $this->pdo->query('SELECT * FROM pattern_matches')->fetchAll(PDO::FETCH_ASSOC);
+        $this->assertCount(1, $matches);
+        $this->assertSame('adapter', $matches[0]['pattern_name']);
+
+        $participants = $this->pdo->query('SELECT * FROM pattern_participants')->fetchAll(PDO::FETCH_ASSOC);
+        $this->assertCount(1, $participants);
+        $this->assertSame('target', $participants[0]['role']);
+        $this->assertSame($entityId, (int) $participants[0]['entity_id']);
+    }
+
+    public function testRecordReplacesPreviousMatches(): void
+    {
+        $oldId = $this->command->insertEntity('App\\Old', 'Old', 'interface', null, null, null, []);
+        $newId = $this->command->insertEntity('App\\New', 'New', 'interface', null, null, null, []);
+
+        $pattern = new PatternInterfaceStub('adapter', ['target']);
+
+        $persister = new MatchPersister($this->pdo);
+
+        $persister->record([
+            new PatternMatch(
+                pattern: $pattern,
+                participants: [new PatternParticipant(role: 'target', entityId: $oldId)],
+            ),
+        ]);
+
+        $persister->record([
+            new PatternMatch(
+                pattern: $pattern,
+                participants: [new PatternParticipant(role: 'target', entityId: $newId)],
+            ),
+        ]);
+
+        $matches = $this->pdo->query('SELECT * FROM pattern_matches')->fetchAll(PDO::FETCH_ASSOC);
+        $this->assertCount(1, $matches);
+    }
+
+    public function testRecordEmptyArrayClearsMatches(): void
+    {
+        $eid = $this->command->insertEntity('App\\Target', 'Target', 'interface', null, null, null, []);
+        $pattern = new PatternInterfaceStub('adapter', ['target']);
+        $persister = new MatchPersister($this->pdo);
+
+        $persister->record([
+            new PatternMatch(
+                pattern: $pattern,
+                participants: [new PatternParticipant(role: 'target', entityId: $eid)],
+            ),
+        ]);
+
+        $persister->record([]);
+
+        $matches = $this->pdo->query('SELECT * FROM pattern_matches')->fetchAll(PDO::FETCH_ASSOC);
+        $this->assertCount(0, $matches);
+    }
+
+}

tests/Unit/Detector/Pattern/Pipeline/PatternRegistryTest.php
@@ -1,0 +1,51 @@
+<?php declare(strict_types=1);
+
+namespace SineFine\Ponymator\Tests\Unit\Detector\Pattern\Pipeline;
+
+use PHPUnit\Framework\TestCase;
+use SineFine\Ponymator\Detector\Pattern\Engine\PatternRegistry;
+use SineFine\Ponymator\Tests\Unit\Detector\Pattern\Stub\PatternInterfaceStub;
+
+final class PatternRegistryTest extends TestCase
+{
+    public function testRegisterAndRetrieve(): void
+    {
+        $registry = new PatternRegistry();
+        $pattern = new PatternInterfaceStub('test', ['a']);
+
+        $registry->register($pattern);
+
+        $this->assertSame($pattern, $registry->get('test'));
+    }
+
+    public function testGetReturnsNullForUnknown(): void
+    {
+        $registry = new PatternRegistry();
+        $this->assertNull($registry->get('nonexistent'));
+    }
+
+    public function testAllReturnsRegisteredPatterns(): void
+    {
+        $p1 = new PatternInterfaceStub('a', ['x']);
+        $p2 = new PatternInterfaceStub('b', ['y']);
+
+        $registry = new PatternRegistry([$p1, $p2]);
+
+        $all = $registry->all();
+        $this->assertCount(2, $all);
+        $this->assertContains($p1, $all);
+        $this->assertContains($p2, $all);
+    }
+
+    public function testRegisterOverwritesDuplicate(): void
+    {
+        $p1 = new PatternInterfaceStub('test', ['a']);
+        $p2 = new PatternInterfaceStub('test', ['b']);
+
+        $registry = new PatternRegistry([$p1]);
+        $registry->register($p2);
+
+        $this->assertSame($p2, $registry->get('test'));
+        $this->assertCount(1, $registry->all());
+    }
+}

tests/Unit/Detector/Pattern/Stub/PatternInterfaceStub.php
@@ -1,0 +1,34 @@
+<?php declare(strict_types=1);
+
+namespace SineFine\Ponymator\Tests\Unit\Detector\Pattern\Stub;
+
+use SineFine\Ponymator\Detector\Pattern\Catalog\PatternInterface;
+
+final class PatternInterfaceStub implements PatternInterface
+{
+    /**
+     * @param string   $name
+     * @param string[] $roles
+     */
+    public function __construct(
+        private string $name,
+        private array $roles,
+        private string $sql = '',
+    ) {
+    }
+
+    public function name(): string
+    {
+        return $this->name;
+    }
+
+    public function roles(): array
+    {
+        return $this->roles;
+    }
+
+    public function candidateSql(): string
+    {
+        return $this->sql;
+    }
+}

sinefineinfinitum/mnemosyne

src/Detector/Pattern/Engine/CandidateCollector.php
@@ -1,0 +1,88 @@
+<?php declare(strict_types=1);
+
+namespace SineFine\Mnemosyne\Detector\Pattern\Engine;
+
+use PDO;
+use SineFine\Mnemosyne\Detector\Pattern\Catalog\PatternInterface;
+use SineFine\Mnemosyne\Detector\Pattern\Model\PatternMatch;
+use SineFine\Mnemosyne\Detector\Pattern\Model\PatternParticipant;
+
+/**
+ * Collects candidate pattern matches by executing SQL from PatternInterface::candidateSql().
+ *
+ * TRUST BOUNDARY: The SQL executed here originates exclusively from built-in
+ * PatternInterface implementations registered in PatternRegistry. These are
+ * compile-time constants, not user-supplied input. If the pattern catalog is
+ * extended to load patterns from external sources (plugins, config files, etc.),
+ * the SQL must be validated before execution (e.g., read-only assertion, EXPLAIN
+ * pre-check, or allowlist of query structures).
+ */
+final class CandidateCollector
+{
+    public function __construct(
+        private PDO              $pdo,
+        private PatternInterface $pattern,
+    ) {
+    }
+
+    /**
+     * @return PatternMatch[]
+     */
+    public function collect(): array
+    {
+        $sql = $this->pattern->candidateSql();
+
+        if (empty($sql)) {
+            return [];
+        }
+
+        return $this->collectFromSql($sql);
+    }
+
+    /**
+     * @return PatternMatch[]
+     */
+    private function collectFromSql(string $sql): array
+    {
+        $stmt = $this->pdo->query($sql);
+        if ($stmt === false) {
+            return [];
+        }
+        /** @var list<array<string, mixed>> $rows */
+        $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        /** @var array<int, list<array{entityId: int, role: string}>> $grouped */
+        $grouped = [];
+        foreach ($rows as $row) {
+            $matchId = isset($row['match_id']) ? (int) $row['match_id'] : 0;
+            $entityId = isset($row['entity_id']) ? (int) $row['entity_id'] : 0;
+            if ($matchId === 0 || $entityId === 0) {
+                continue;
+            }
+
+            $role = isset($row['role']) && is_string($row['role']) ? $row['role'] : '';
+            if ($role === '') {
+                continue;
+            }
+            $dedupeKey = $entityId . "\0" . $role;
+            $grouped[$matchId][$dedupeKey] = ['entityId' => $entityId, 'role' => $role];
+        }
+
+        $matches = [];
+        foreach ($grouped as $participants) {
+            $patternParticipants = [];
+            foreach ($participants as $p) {
+                $patternParticipants[] = new PatternParticipant(role: $p['role'], entityId: $p['entityId']);
+            }
+
+            if ($patternParticipants !== []) {
+                $matches[] = new PatternMatch(
+                    pattern: $this->pattern,
+                    participants: $patternParticipants,
+                );
+            }
+        }
+
+        return $matches;
+    }
+}

src/Detector/Pattern/Engine/Engine.php
@@ -1,0 +1,45 @@
+<?php declare(strict_types=1);
+
+namespace SineFine\Mnemosyne\Detector\Pattern\Engine;
+
+use PDO;
+use SineFine\Mnemosyne\Detector\Pattern\Model\PatternResult;
+use Throwable;
+
+final class Engine
+{
+    public function __construct(
+        private PatternRegistry $registry,
+        private PDO $pdo,
+        private ?PDO $readOnlyPdo = null,
+    ) {
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function run(): PatternResult
+    {
+        $allMatches = [];
+        $errors = [];
+        $detectionPdo = $this->readOnlyPdo ?? $this->pdo;
+
+        foreach ($this->registry->all() as $pattern) {
+            try {
+                $collector = new CandidateCollector($detectionPdo, $pattern);
+                $matches = $collector->collect();
+                $allMatches = [...$allMatches, ...$matches];
+            } catch (\PDOException $e) {
+                $errors[] = "Pattern '{$pattern->name()}': " . $e->getMessage();
+            }
+        }
+
+        $persister = new MatchPersister($this->pdo);
+        $persister->record($allMatches);
+
+        return new PatternResult(
+            matches: $allMatches,
+            errors: $errors,
+        );
+    }
+}

src/Detector/Pattern/Engine/MatchPersister.php
@@ -1,0 +1,60 @@
+<?php declare(strict_types=1);
+
+namespace SineFine\Mnemosyne\Detector\Pattern\Engine;
+
+use PDO;
+use SineFine\Mnemosyne\Detector\Pattern\Model\PatternMatch;
+use Throwable;
+
+final class MatchPersister
+{
+    public function __construct(
+        private PDO $pdo,
+    ) {
+    }
+
+    /**
+     * @param  PatternMatch[] $matches
+     * @throws Throwable
+     */
+    public function record(array $matches): void
+    {
+        $this->pdo->beginTransaction();
+        try {
+            $this->pdo->exec('DELETE FROM pattern_participants');
+            $this->pdo->exec('DELETE FROM pattern_matches');
+
+            $insertMatch = $this->pdo->prepare(
+                'INSERT INTO pattern_matches (pattern_name) VALUES (:name)'
+            );
+            $insertPart = $this->pdo->prepare(
+                'INSERT INTO pattern_participants (match_id, entity_id, role) VALUES (:match_id, :entity_id, :role)'
+            );
+
+            foreach ($matches as $match) {
+                $insertMatch->execute(
+                    [
+                    'name' => $match->pattern->name(),
+                    ]
+                );
+
+                $matchId = (int) $this->pdo->lastInsertId();
+
+                foreach ($match->participants as $participant) {
+                    $insertPart->execute(
+                        [
+                        'match_id' => $matchId,
+                        'entity_id' => $participant->entityId,
+                        'role' => $participant->role,
+                        ]
+                    );
+                }
+            }
+
+            $this->pdo->commit();
+        } catch (Throwable $e) {
+            $this->pdo->rollBack();
+            throw $e;
+        }
+    }
+}

src/Detector/Pattern/Engine/PatternRegistry.php
@@ -1,0 +1,39 @@
+<?php declare(strict_types=1);
+
+namespace SineFine\Mnemosyne\Detector\Pattern\Engine;
+
+use SineFine\Mnemosyne\Detector\Pattern\Catalog\PatternInterface;
+
+final class PatternRegistry
+{
+    /** @var PatternInterface[] */
+    private array $patterns = [];
+
+    /**
+     * @param PatternInterface[] $patterns
+     */
+    public function __construct(array $patterns = [])
+    {
+        foreach ($patterns as $pattern) {
+            $this->register($pattern);
+        }
+    }
+
+    public function register(PatternInterface $pattern): void
+    {
+        $this->patterns[$pattern->name()] = $pattern;
+    }
+
+    public function get(string $name): ?PatternInterface
+    {
+        return $this->patterns[$name] ?? null;
+    }
+
+    /**
+     * @return PatternInterface[]
+     */
+    public function all(): array
+    {
+        return array_values($this->patterns);
+    }
+}

tests/Unit/Detector/Pattern/Pipeline/MatchPersisterTest.php
@@ -1,0 +1,103 @@
+<?php declare(strict_types=1);
+
+namespace SineFine\Mnemosyne\Tests\Unit\Detector\Pattern\Pipeline;
+
+use PDO;
+use PHPUnit\Framework\TestCase;
+use SineFine\Mnemosyne\Detector\Pattern\Engine\MatchPersister;
+use SineFine\Mnemosyne\Detector\Pattern\Model\PatternMatch;
+use SineFine\Mnemosyne\Detector\Pattern\Model\PatternParticipant;
+use SineFine\Mnemosyne\Graph\Experimental\GraphCommand;
+use SineFine\Mnemosyne\Graph\Experimental\GraphQuery;
+use SineFine\Mnemosyne\Graph\Experimental\Schema;
+use SineFine\Mnemosyne\Tests\Unit\Detector\Pattern\Stub\PatternInterfaceStub;
+
+final class MatchPersisterTest extends TestCase
+{
+    private PDO $pdo;
+    private GraphQuery $query;
+    private GraphCommand $command;
+
+    protected function setUp(): void
+    {
+        $this->pdo = new PDO('sqlite::memory:');
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        Schema::create($this->pdo);
+        $this->command = new GraphCommand($this->pdo);
+        $this->query = new GraphQuery($this->pdo);
+    }
+
+    public function testRecordStoresMatches(): void
+    {
+        $entityId = $this->command->insertEntity(
+            'App\\TargetInterface', 'TargetInterface', 'interface', null, null, null, [],
+        );
+
+        $pattern = new PatternInterfaceStub('adapter', ['target']);
+        $match = new PatternMatch(
+            pattern: $pattern,
+            participants: [
+                new PatternParticipant(role: 'target', entityId: $entityId),
+            ],
+        );
+
+        $persister = new MatchPersister($this->pdo);
+        $persister->record([$match]);
+
+        $matches = $this->pdo->query('SELECT * FROM pattern_matches')->fetchAll(PDO::FETCH_ASSOC);
+        $this->assertCount(1, $matches);
+        $this->assertSame('adapter', $matches[0]['pattern_name']);
+
+        $participants = $this->pdo->query('SELECT * FROM pattern_participants')->fetchAll(PDO::FETCH_ASSOC);
+        $this->assertCount(1, $participants);
+        $this->assertSame('target', $participants[0]['role']);
+        $this->assertSame($entityId, (int) $participants[0]['entity_id']);
+    }
+
+    public function testRecordReplacesPreviousMatches(): void
+    {
+        $oldId = $this->command->insertEntity('App\\Old', 'Old', 'interface', null, null, null, []);
+        $newId = $this->command->insertEntity('App\\New', 'New', 'interface', null, null, null, []);
+
+        $pattern = new PatternInterfaceStub('adapter', ['target']);
+
+        $persister = new MatchPersister($this->pdo);
+
+        $persister->record([
+            new PatternMatch(
+                pattern: $pattern,
+                participants: [new PatternParticipant(role: 'target', entityId: $oldId)],
+            ),
+        ]);
+
+        $persister->record([
+            new PatternMatch(
+                pattern: $pattern,
+                participants: [new PatternParticipant(role: 'target', entityId: $newId)],
+            ),
+        ]);
+
+        $matches = $this->pdo->query('SELECT * FROM pattern_matches')->fetchAll(PDO::FETCH_ASSOC);
+        $this->assertCount(1, $matches);
+    }
+
+    public function testRecordEmptyArrayClearsMatches(): void
+    {
+        $eid = $this->command->insertEntity('App\\Target', 'Target', 'interface', null, null, null, []);
+        $pattern = new PatternInterfaceStub('adapter', ['target']);
+        $persister = new MatchPersister($this->pdo);
+
+        $persister->record([
+            new PatternMatch(
+                pattern: $pattern,
+                participants: [new PatternParticipant(role: 'target', entityId: $eid)],
+            ),
+        ]);
+
+        $persister->record([]);
+
+        $matches = $this->pdo->query('SELECT * FROM pattern_matches')->fetchAll(PDO::FETCH_ASSOC);
+        $this->assertCount(0, $matches);
+    }
+
+}

tests/Unit/Detector/Pattern/Pipeline/PatternRegistryTest.php
@@ -1,0 +1,51 @@
+<?php declare(strict_types=1);
+
+namespace SineFine\Mnemosyne\Tests\Unit\Detector\Pattern\Pipeline;
+
+use PHPUnit\Framework\TestCase;
+use SineFine\Mnemosyne\Detector\Pattern\Engine\PatternRegistry;
+use SineFine\Mnemosyne\Tests\Unit\Detector\Pattern\Stub\PatternInterfaceStub;
+
+final class PatternRegistryTest extends TestCase
+{
+    public function testRegisterAndRetrieve(): void
+    {
+        $registry = new PatternRegistry();
+        $pattern = new PatternInterfaceStub('test', ['a']);
+
+        $registry->register($pattern);
+
+        $this->assertSame($pattern, $registry->get('test'));
+    }
+
+    public function testGetReturnsNullForUnknown(): void
+    {
+        $registry = new PatternRegistry();
+        $this->assertNull($registry->get('nonexistent'));
+    }
+
+    public function testAllReturnsRegisteredPatterns(): void
+    {
+        $p1 = new PatternInterfaceStub('a', ['x']);
+        $p2 = new PatternInterfaceStub('b', ['y']);
+
+        $registry = new PatternRegistry([$p1, $p2]);
+
+        $all = $registry->all();
+        $this->assertCount(2, $all);
+        $this->assertContains($p1, $all);
+        $this->assertContains($p2, $all);
+    }
+
+    public function testRegisterOverwritesDuplicate(): void
+    {
+        $p1 = new PatternInterfaceStub('test', ['a']);
+        $p2 = new PatternInterfaceStub('test', ['b']);
+
+        $registry = new PatternRegistry([$p1]);
+        $registry->register($p2);
+
+        $this->assertSame($p2, $registry->get('test'));
+        $this->assertCount(1, $registry->all());
+    }
+}

tests/Unit/Detector/Pattern/Stub/PatternInterfaceStub.php
@@ -1,0 +1,34 @@
+<?php declare(strict_types=1);
+
+namespace SineFine\Mnemosyne\Tests\Unit\Detector\Pattern\Stub;
+
+use SineFine\Mnemosyne\Detector\Pattern\Catalog\PatternInterface;
+
+final class PatternInterfaceStub implements PatternInterface
+{
+    /**
+     * @param string   $name
+     * @param string[] $roles
+     */
+    public function __construct(
+        private string $name,
+        private array $roles,
+        private string $sql = '',
+    ) {
+    }
+
+    public function name(): string
+    {
+        return $this->name;
+    }
+
+    public function roles(): array
+    {
+        return $this->roles;
+    }
+
+    public function candidateSql(): string
+    {
+        return $this->sql;
+    }
+}