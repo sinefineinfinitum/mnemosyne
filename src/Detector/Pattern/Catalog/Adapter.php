sinefineinfinitum/ponymator

feat: add pattern catalog

src/Detector/Pattern/Catalog/Adapter.php
@@ -1,0 +1,72 @@
+<?php declare(strict_types=1);
+
+namespace SineFine\Ponymator\Detector\Pattern\Catalog;
+
+final class Adapter implements PatternInterface
+{
+    public function name(): string
+    {
+        return 'adapter';
+    }
+
+    /**
+     * @return string[]
+     */
+    public function roles(): array
+    {
+        return ['Adapter', 'Target', 'Adaptee'];
+    }
+
+    private const CTE_BASE = <<<'SQL'
+            WITH base AS (
+                SELECT adapter.id AS adapter_id,
+                       iface.id   AS target_id,
+                       adaptee.id AS adaptee_id
+                FROM entities adapter
+                JOIN relationships r_impl
+                  ON r_impl.source_id = adapter.id AND r_impl.type = 'implements'
+                JOIN entities iface
+                  ON iface.id = r_impl.target_id AND iface.type = 'interface'
+                JOIN relationships r_dep
+                  ON r_dep.source_id = adapter.id
+                 AND r_dep.type IN ('dependency', 'creates', 'creates_strong')
+                 AND r_dep.target_id IS NOT NULL
+                 AND r_dep.target_id != iface.id
+                 AND r_dep.target_id NOT IN (
+                     SELECT r2.target_id FROM relationships r2
+                     WHERE r2.source_id = iface.id AND r2.type IN ('extends', 'implements')
+                 )
+                JOIN entities adaptee ON adaptee.id = r_dep.target_id
+                WHERE adapter.type = 'class'
+            )
+            SQL;
+
+    private const SELECT_ADAPTER = <<<'SQL'
+            SELECT DENSE_RANK() OVER (ORDER BY adapter_id, target_id, adaptee_id) AS match_id,
+                   adapter_id AS entity_id, 'Adapter' AS role
+            FROM base
+            SQL;
+
+    private const SELECT_TARGET = <<<'SQL'
+            SELECT DENSE_RANK() OVER (ORDER BY adapter_id, target_id, adaptee_id) AS match_id,
+                   target_id AS entity_id, 'Target' AS role
+            FROM base
+            SQL;
+
+    private const SELECT_ADAPTEE = <<<'SQL'
+            SELECT DENSE_RANK() OVER (ORDER BY adapter_id, target_id, adaptee_id) AS match_id,
+                   adaptee_id AS entity_id, 'Adaptee' AS role
+            FROM base
+            SQL;
+
+    public function candidateSql(): string
+    {
+        return
+            self::CTE_BASE . "\n" .
+            self::SELECT_ADAPTER . "\n" .
+            "UNION ALL\n" .
+            self::SELECT_TARGET . "\n" .
+            "UNION ALL\n" .
+            self::SELECT_ADAPTEE;
+    }
+}

src/Detector/Pattern/Catalog/Decorator.php
@@ -1,0 +1,52 @@
+<?php declare(strict_types=1);
+
+namespace SineFine\Ponymator\Detector\Pattern\Catalog;
+
+final class Decorator implements PatternInterface
+{
+    public function name(): string
+    {
+        return 'decorator';
+    }
+
+    /**
+     * @return string[]
+     */
+    public function roles(): array
+    {
+        return ['Component', 'Decorator'];
+    }
+
+    private const CTE_IMPLEMENTS = <<<'SQL'
+            WITH impl_pairs AS (
+                SELECT c.id AS component_id,
+                       d.id AS decorator_id
+                FROM entities c
+                JOIN relationships r ON r.target_id = c.id AND r.type = 'implements'
+                JOIN entities d ON r.source_id = d.id
+                WHERE c.type = 'interface'
+                  AND d.type = 'class'
+            )
+            SQL;
+
+    private const SELECT_COMPONENT = <<<'SQL'
+            SELECT DENSE_RANK() OVER (ORDER BY component_id, decorator_id) AS match_id,
+                   component_id AS entity_id, 'Component' AS role
+            FROM impl_pairs
+            SQL;
+
+    private const SELECT_DECORATOR = <<<'SQL'
+            SELECT DENSE_RANK() OVER (ORDER BY component_id, decorator_id) AS match_id,
+                   decorator_id AS entity_id, 'Decorator' AS role
+            FROM impl_pairs
+            SQL;
+
+    public function candidateSql(): string
+    {
+        return
+            self::CTE_IMPLEMENTS . "\n" .
+            self::SELECT_COMPONENT . "\n" .
+            "UNION ALL\n" .
+            self::SELECT_DECORATOR;
+    }
+}

src/Detector/Pattern/Catalog/PatternInterface.php
@@ -1,0 +1,24 @@
+<?php declare(strict_types=1);
+
+namespace SineFine\Ponymator\Detector\Pattern\Catalog;
+
+interface PatternInterface
+{
+    public function name(): string;
+
+    /**
+     * @return string[]
+     */
+    public function roles(): array;
+
+    /**
+     * SQL must return rows with columns:
+     *   - match_id  (int)  — groups participants belonging to the same pattern instance
+     *   - entity_id (int)  — the participant entity
+     *   - role      (string) — the role this entity plays in the pattern
+     *
+     * Use DENSE_RANK() OVER (ORDER BY ...) to generate deterministic match_ids
+     * from sorted participant entity IDs.
+     */
+    public function candidateSql(): string;
+}

src/Detector/Pattern/Catalog/Singleton.php
@@ -1,0 +1,52 @@
+<?php declare(strict_types=1);
+
+namespace SineFine\Ponymator\Detector\Pattern\Catalog;
+
+final class Singleton implements PatternInterface
+{
+    public function name(): string
+    {
+        return 'singleton';
+    }
+
+    /**
+     * @return string[]
+     */
+    public function roles(): array
+    {
+        return ['Singleton'];
+    }
+
+    public function candidateSql(): string
+    {
+        return <<<'SQL'
+            SELECT e.id AS match_id, e.id AS entity_id, 'Singleton' AS role
+            FROM entities e
+            WHERE e.type = 'class'
+              AND e.is_abstract = 0
+              AND EXISTS (
+                SELECT 1 FROM members
+                WHERE entity_id = e.id
+                  AND member_type = 'method'
+                  AND name = '__construct'
+                  AND visibility = 'private'
+              )
+              AND EXISTS (
+                SELECT 1 FROM members m
+                WHERE m.entity_id = e.id
+                  AND m.member_type = 'property'
+                  AND m.visibility = 'private'
+                  AND m.is_static = 1
+                  AND (m.declared_type IN ('self', 'static') OR m.declared_type = e.fqn)
+              )
+              AND EXISTS (
+                SELECT 1 FROM members m
+                WHERE m.entity_id = e.id
+                  AND m.member_type = 'method'
+                  AND m.visibility = 'public'
+                  AND m.is_static = 1
+                  AND (m.return_type IN ('self', 'static') OR m.return_type = e.fqn)
+              )
+            SQL;
+    }
+}

src/Detector/Pattern/Catalog/Strategy.php
@@ -1,0 +1,89 @@
+<?php declare(strict_types=1);
+
+namespace SineFine\Ponymator\Detector\Pattern\Catalog;
+
+final class Strategy implements PatternInterface
+{
+    public function name(): string
+    {
+        return 'strategy';
+    }
+
+    /**
+     * @return string[]
+     */
+    public function roles(): array
+    {
+        return ['Strategy', 'ConcreteStrategy', 'Context'];
+    }
+
+    private const CTE_IMPLS = <<<'SQL'
+            WITH impl_pairs AS (
+                SELECT s.id    AS strategy_id,
+                       impl.id AS concrete_id
+                FROM entities s
+                JOIN relationships r ON r.type = 'implements' AND r.target_id = s.id
+                JOIN entities impl ON impl.id = r.source_id
+                WHERE s.type = 'interface'
+            ),
+            multi_impls AS (
+                SELECT strategy_id
+                FROM impl_pairs
+                GROUP BY strategy_id
+                HAVING COUNT(DISTINCT concrete_id) >= 2
+            )
+            SQL;
+
+    private const SELECT_STRATEGY_FROM_IMPLS = <<<'SQL'
+            SELECT DENSE_RANK() OVER (ORDER BY ip.strategy_id, ip.concrete_id) AS match_id,
+                   ip.strategy_id AS entity_id, 'Strategy' AS role
+            FROM impl_pairs ip
+            JOIN multi_impls mi ON mi.strategy_id = ip.strategy_id
+            SQL;
+
+    private const SELECT_CONCRETE = <<<'SQL'
+            SELECT DENSE_RANK() OVER (ORDER BY ip.strategy_id, ip.concrete_id) AS match_id,
+                   ip.concrete_id AS entity_id, 'ConcreteStrategy' AS role
+            FROM impl_pairs ip
+            JOIN multi_impls mi ON mi.strategy_id = ip.strategy_id
+            SQL;
+
+    private const CTE_CTX = <<<'SQL'
+            ctx_pairs AS (
+                SELECT s.id   AS strategy_id,
+                       ctx.id AS context_id
+                FROM entities s
+                JOIN relationships r ON r.type = 'dependency' AND r.target_id = s.id
+                JOIN entities ctx ON ctx.id = r.source_id
+                WHERE s.type = 'interface'
+            )
+            SQL;
+
+    private const SELECT_STRATEGY_FROM_CTX = <<<'SQL'
+            SELECT DENSE_RANK() OVER (ORDER BY cp.strategy_id, cp.context_id) AS match_id,
+                   cp.strategy_id AS entity_id, 'Strategy' AS role
+            FROM ctx_pairs cp
+            JOIN multi_impls mi ON mi.strategy_id = cp.strategy_id
+            SQL;
+
+    private const SELECT_CONTEXT = <<<'SQL'
+            SELECT DENSE_RANK() OVER (ORDER BY cp.strategy_id, cp.context_id) AS match_id,
+                   cp.context_id AS entity_id, 'Context' AS role
+            FROM ctx_pairs cp
+            JOIN multi_impls mi ON mi.strategy_id = cp.strategy_id
+            SQL;
+
+    public function candidateSql(): string
+    {
+        return
+            self::CTE_IMPLS . ",\n" .
+            self::CTE_CTX . "\n" .
+            self::SELECT_STRATEGY_FROM_IMPLS . "\n" .
+            "UNION ALL\n" .
+            self::SELECT_CONCRETE . "\n" .
+            "UNION ALL\n" .
+            self::SELECT_STRATEGY_FROM_CTX . "\n" .
+            "UNION ALL\n" .
+            self::SELECT_CONTEXT;
+    }
+}

src/Detector/Pattern/Catalog/TemplateMethod.php
@@ -1,0 +1,76 @@
+<?php declare(strict_types=1);
+
+namespace SineFine\Ponymator\Detector\Pattern\Catalog;
+
+final class TemplateMethod implements PatternInterface
+{
+    public function name(): string
+    {
+        return 'template_method';
+    }
+
+    /**
+     * @return string[]
+     */
+    public function roles(): array
+    {
+        return ['AbstractClass', 'ConcreteClass'];
+    }
+
+    private const CTE_BASE = <<<'SQL'
+            WITH base AS (
+                SELECT DISTINCT
+                    abs.id   AS abstract_id,
+                    child.id AS concrete_id
+                FROM entities abs
+                JOIN relationships r
+                  ON r.target_id = abs.id AND r.type = 'extends'
+                JOIN entities child ON r.source_id = child.id
+                WHERE abs.type = 'class'
+                  AND abs.is_abstract = 1
+                  AND child.type = 'class'
+                  AND child.is_abstract = 0
+                  AND EXISTS (
+                    SELECT 1 FROM members
+                    WHERE entity_id = abs.id
+                      AND member_type = 'method'
+                      AND is_abstract = 1
+                      AND visibility IN ('protected', 'public')
+                  )
+                  AND EXISTS (
+                    SELECT 1 FROM members
+                    WHERE entity_id = abs.id
+                      AND member_type = 'method'
+                      AND is_abstract = 0
+                      AND visibility = 'public'
+                  )
+                  AND EXISTS (
+                    SELECT 1 FROM relationships r2
+                    WHERE r2.source_id = abs.id
+                      AND r2.source_member_id IS NOT NULL
+                      AND r2.type LIKE 'call_%'
+                  )
+            )
+            SQL;
+
+    private const SELECT_ABSTRACT = <<<'SQL'
+            SELECT DENSE_RANK() OVER (ORDER BY abstract_id, concrete_id) AS match_id,
+                   abstract_id AS entity_id, 'AbstractClass' AS role
+            FROM base
+            SQL;
+
+    private const SELECT_CONCRETE = <<<'SQL'
+            SELECT DENSE_RANK() OVER (ORDER BY abstract_id, concrete_id) AS match_id,
+                   concrete_id AS entity_id, 'ConcreteClass' AS role
+            FROM base
+            SQL;
+
+    public function candidateSql(): string
+    {
+        return
+            self::CTE_BASE . "\n" .
+            self::SELECT_ABSTRACT . "\n" .
+            "UNION ALL\n" .
+            self::SELECT_CONCRETE;
+    }
+}

tests/Unit/Detector/Pattern/Builtin/BuilderTest.php
@@ -1,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace SineFine\Ponymator\Tests\Unit\Detector\Pattern\Builtin;
+
+use PHPUnit\Framework\TestCase;
+use SineFine\Ponymator\Detector\Pattern\Catalog\Builder;
+
+final class BuilderTest extends TestCase
+{
+    private Builder $pattern;
+
+    protected function setUp(): void
+    {
+        $this->pattern = new Builder();
+    }
+
+    public function testName(): void
+    {
+        $this->assertSame('builder', $this->pattern->name());
+    }
+
+    public function testRoles(): void
+    {
+        $this->assertSame(['Builder', 'ConcreteBuilder', 'Director'], $this->pattern->roles());
+    }
+
+    public function testCandidateSql(): void
+    {
+        $this->assertNotEmpty($this->pattern->candidateSql());
+    }
+}

tests/Unit/Detector/Pattern/Builtin/StrategyTest.php
@@ -1,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace SineFine\Ponymator\Tests\Unit\Detector\Pattern\Builtin;
+
+use PHPUnit\Framework\TestCase;
+use SineFine\Ponymator\Detector\Pattern\Catalog\Strategy;
+
+final class StrategyTest extends TestCase
+{
+    private Strategy $pattern;
+
+    protected function setUp(): void
+    {
+        $this->pattern = new Strategy();
+    }
+
+    public function testName(): void
+    {
+        $this->assertSame('strategy', $this->pattern->name());
+    }
+
+    public function testRoles(): void
+    {
+        $this->assertSame(['Strategy', 'ConcreteStrategy', 'Context'], $this->pattern->roles());
+    }
+
+    public function testCandidateSql(): void
+    {
+        $this->assertNotEmpty($this->pattern->candidateSql());
+    }
+}

sinefineinfinitum/mnemosyne

src/Detector/Pattern/Catalog/Adapter.php
@@ -1,0 +1,72 @@
+<?php declare(strict_types=1);
+
+namespace SineFine\Mnemosyne\Detector\Pattern\Catalog;
+
+final class Adapter implements PatternInterface
+{
+    public function name(): string
+    {
+        return 'adapter';
+    }
+
+    /**
+     * @return string[]
+     */
+    public function roles(): array
+    {
+        return ['Adapter', 'Target', 'Adaptee'];
+    }
+
+    private const CTE_BASE = <<<'SQL'
+            WITH base AS (
+                SELECT adapter.id AS adapter_id,
+                       iface.id   AS target_id,
+                       adaptee.id AS adaptee_id
+                FROM entities adapter
+                JOIN relationships r_impl
+                  ON r_impl.source_id = adapter.id AND r_impl.type = 'implements'
+                JOIN entities iface
+                  ON iface.id = r_impl.target_id AND iface.type = 'interface'
+                JOIN relationships r_dep
+                  ON r_dep.source_id = adapter.id
+                 AND r_dep.type IN ('dependency', 'creates', 'creates_strong')
+                 AND r_dep.target_id IS NOT NULL
+                 AND r_dep.target_id != iface.id
+                 AND r_dep.target_id NOT IN (
+                     SELECT r2.target_id FROM relationships r2
+                     WHERE r2.source_id = iface.id AND r2.type IN ('extends', 'implements')
+                 )
+                JOIN entities adaptee ON adaptee.id = r_dep.target_id
+                WHERE adapter.type = 'class'
+            )
+            SQL;
+
+    private const SELECT_ADAPTER = <<<'SQL'
+            SELECT DENSE_RANK() OVER (ORDER BY adapter_id, target_id, adaptee_id) AS match_id,
+                   adapter_id AS entity_id, 'Adapter' AS role
+            FROM base
+            SQL;
+
+    private const SELECT_TARGET = <<<'SQL'
+            SELECT DENSE_RANK() OVER (ORDER BY adapter_id, target_id, adaptee_id) AS match_id,
+                   target_id AS entity_id, 'Target' AS role
+            FROM base
+            SQL;
+
+    private const SELECT_ADAPTEE = <<<'SQL'
+            SELECT DENSE_RANK() OVER (ORDER BY adapter_id, target_id, adaptee_id) AS match_id,
+                   adaptee_id AS entity_id, 'Adaptee' AS role
+            FROM base
+            SQL;
+
+    public function candidateSql(): string
+    {
+        return
+            self::CTE_BASE . "\n" .
+            self::SELECT_ADAPTER . "\n" .
+            "UNION ALL\n" .
+            self::SELECT_TARGET . "\n" .
+            "UNION ALL\n" .
+            self::SELECT_ADAPTEE;
+    }
+}

src/Detector/Pattern/Catalog/Decorator.php
@@ -1,0 +1,52 @@
+<?php declare(strict_types=1);
+
+namespace SineFine\Mnemosyne\Detector\Pattern\Catalog;
+
+final class Decorator implements PatternInterface
+{
+    public function name(): string
+    {
+        return 'decorator';
+    }
+
+    /**
+     * @return string[]
+     */
+    public function roles(): array
+    {
+        return ['Component', 'Decorator'];
+    }
+
+    private const CTE_IMPLEMENTS = <<<'SQL'
+            WITH impl_pairs AS (
+                SELECT c.id AS component_id,
+                       d.id AS decorator_id
+                FROM entities c
+                JOIN relationships r ON r.target_id = c.id AND r.type = 'implements'
+                JOIN entities d ON r.source_id = d.id
+                WHERE c.type = 'interface'
+                  AND d.type = 'class'
+            )
+            SQL;
+
+    private const SELECT_COMPONENT = <<<'SQL'
+            SELECT DENSE_RANK() OVER (ORDER BY component_id, decorator_id) AS match_id,
+                   component_id AS entity_id, 'Component' AS role
+            FROM impl_pairs
+            SQL;
+
+    private const SELECT_DECORATOR = <<<'SQL'
+            SELECT DENSE_RANK() OVER (ORDER BY component_id, decorator_id) AS match_id,
+                   decorator_id AS entity_id, 'Decorator' AS role
+            FROM impl_pairs
+            SQL;
+
+    public function candidateSql(): string
+    {
+        return
+            self::CTE_IMPLEMENTS . "\n" .
+            self::SELECT_COMPONENT . "\n" .
+            "UNION ALL\n" .
+            self::SELECT_DECORATOR;
+    }
+}

src/Detector/Pattern/Catalog/PatternInterface.php
@@ -1,0 +1,24 @@
+<?php declare(strict_types=1);
+
+namespace SineFine\Mnemosyne\Detector\Pattern\Catalog;
+
+interface PatternInterface
+{
+    public function name(): string;
+
+    /**
+     * @return string[]
+     */
+    public function roles(): array;
+
+    /**
+     * SQL must return rows with columns:
+     *   - match_id  (int)  — groups participants belonging to the same pattern instance
+     *   - entity_id (int)  — the participant entity
+     *   - role      (string) — the role this entity plays in the pattern
+     *
+     * Use DENSE_RANK() OVER (ORDER BY ...) to generate deterministic match_ids
+     * from sorted participant entity IDs.
+     */
+    public function candidateSql(): string;
+}

src/Detector/Pattern/Catalog/Singleton.php
@@ -1,0 +1,52 @@
+<?php declare(strict_types=1);
+
+namespace SineFine\Mnemosyne\Detector\Pattern\Catalog;
+
+final class Singleton implements PatternInterface
+{
+    public function name(): string
+    {
+        return 'singleton';
+    }
+
+    /**
+     * @return string[]
+     */
+    public function roles(): array
+    {
+        return ['Singleton'];
+    }
+
+    public function candidateSql(): string
+    {
+        return <<<'SQL'
+            SELECT e.id AS match_id, e.id AS entity_id, 'Singleton' AS role
+            FROM entities e
+            WHERE e.type = 'class'
+              AND e.is_abstract = 0
+              AND EXISTS (
+                SELECT 1 FROM members
+                WHERE entity_id = e.id
+                  AND member_type = 'method'
+                  AND name = '__construct'
+                  AND visibility = 'private'
+              )
+              AND EXISTS (
+                SELECT 1 FROM members m
+                WHERE m.entity_id = e.id
+                  AND m.member_type = 'property'
+                  AND m.visibility = 'private'
+                  AND m.is_static = 1
+                  AND (m.declared_type IN ('self', 'static') OR m.declared_type = e.fqn)
+              )
+              AND EXISTS (
+                SELECT 1 FROM members m
+                WHERE m.entity_id = e.id
+                  AND m.member_type = 'method'
+                  AND m.visibility = 'public'
+                  AND m.is_static = 1
+                  AND (m.return_type IN ('self', 'static') OR m.return_type = e.fqn)
+              )
+            SQL;
+    }
+}

src/Detector/Pattern/Catalog/Strategy.php
@@ -1,0 +1,89 @@
+<?php declare(strict_types=1);
+
+namespace SineFine\Mnemosyne\Detector\Pattern\Catalog;
+
+final class Strategy implements PatternInterface
+{
+    public function name(): string
+    {
+        return 'strategy';
+    }
+
+    /**
+     * @return string[]
+     */
+    public function roles(): array
+    {
+        return ['Strategy', 'ConcreteStrategy', 'Context'];
+    }
+
+    private const CTE_IMPLS = <<<'SQL'
+            WITH impl_pairs AS (
+                SELECT s.id    AS strategy_id,
+                       impl.id AS concrete_id
+                FROM entities s
+                JOIN relationships r ON r.type = 'implements' AND r.target_id = s.id
+                JOIN entities impl ON impl.id = r.source_id
+                WHERE s.type = 'interface'
+            ),
+            multi_impls AS (
+                SELECT strategy_id
+                FROM impl_pairs
+                GROUP BY strategy_id
+                HAVING COUNT(DISTINCT concrete_id) >= 2
+            )
+            SQL;
+
+    private const SELECT_STRATEGY_FROM_IMPLS = <<<'SQL'
+            SELECT DENSE_RANK() OVER (ORDER BY ip.strategy_id, ip.concrete_id) AS match_id,
+                   ip.strategy_id AS entity_id, 'Strategy' AS role
+            FROM impl_pairs ip
+            JOIN multi_impls mi ON mi.strategy_id = ip.strategy_id
+            SQL;
+
+    private const SELECT_CONCRETE = <<<'SQL'
+            SELECT DENSE_RANK() OVER (ORDER BY ip.strategy_id, ip.concrete_id) AS match_id,
+                   ip.concrete_id AS entity_id, 'ConcreteStrategy' AS role
+            FROM impl_pairs ip
+            JOIN multi_impls mi ON mi.strategy_id = ip.strategy_id
+            SQL;
+
+    private const CTE_CTX = <<<'SQL'
+            ctx_pairs AS (
+                SELECT s.id   AS strategy_id,
+                       ctx.id AS context_id
+                FROM entities s
+                JOIN relationships r ON r.type = 'dependency' AND r.target_id = s.id
+                JOIN entities ctx ON ctx.id = r.source_id
+                WHERE s.type = 'interface'
+            )
+            SQL;
+
+    private const SELECT_STRATEGY_FROM_CTX = <<<'SQL'
+            SELECT DENSE_RANK() OVER (ORDER BY cp.strategy_id, cp.context_id) AS match_id,
+                   cp.strategy_id AS entity_id, 'Strategy' AS role
+            FROM ctx_pairs cp
+            JOIN multi_impls mi ON mi.strategy_id = cp.strategy_id
+            SQL;
+
+    private const SELECT_CONTEXT = <<<'SQL'
+            SELECT DENSE_RANK() OVER (ORDER BY cp.strategy_id, cp.context_id) AS match_id,
+                   cp.context_id AS entity_id, 'Context' AS role
+            FROM ctx_pairs cp
+            JOIN multi_impls mi ON mi.strategy_id = cp.strategy_id
+            SQL;
+
+    public function candidateSql(): string
+    {
+        return
+            self::CTE_IMPLS . ",\n" .
+            self::CTE_CTX . "\n" .
+            self::SELECT_STRATEGY_FROM_IMPLS . "\n" .
+            "UNION ALL\n" .
+            self::SELECT_CONCRETE . "\n" .
+            "UNION ALL\n" .
+            self::SELECT_STRATEGY_FROM_CTX . "\n" .
+            "UNION ALL\n" .
+            self::SELECT_CONTEXT;
+    }
+}

src/Detector/Pattern/Catalog/TemplateMethod.php
@@ -1,0 +1,76 @@
+<?php declare(strict_types=1);
+
+namespace SineFine\Mnemosyne\Detector\Pattern\Catalog;
+
+final class TemplateMethod implements PatternInterface
+{
+    public function name(): string
+    {
+        return 'template_method';
+    }
+
+    /**
+     * @return string[]
+     */
+    public function roles(): array
+    {
+        return ['AbstractClass', 'ConcreteClass'];
+    }
+
+    private const CTE_BASE = <<<'SQL'
+            WITH base AS (
+                SELECT DISTINCT
+                    abs.id   AS abstract_id,
+                    child.id AS concrete_id
+                FROM entities abs
+                JOIN relationships r
+                  ON r.target_id = abs.id AND r.type = 'extends'
+                JOIN entities child ON r.source_id = child.id
+                WHERE abs.type = 'class'
+                  AND abs.is_abstract = 1
+                  AND child.type = 'class'
+                  AND child.is_abstract = 0
+                  AND EXISTS (
+                    SELECT 1 FROM members
+                    WHERE entity_id = abs.id
+                      AND member_type = 'method'
+                      AND is_abstract = 1
+                      AND visibility IN ('protected', 'public')
+                  )
+                  AND EXISTS (
+                    SELECT 1 FROM members
+                    WHERE entity_id = abs.id
+                      AND member_type = 'method'
+                      AND is_abstract = 0
+                      AND visibility = 'public'
+                  )
+                  AND EXISTS (
+                    SELECT 1 FROM relationships r2
+                    WHERE r2.source_id = abs.id
+                      AND r2.source_member_id IS NOT NULL
+                      AND r2.type LIKE 'call_%'
+                  )
+            )
+            SQL;
+
+    private const SELECT_ABSTRACT = <<<'SQL'
+            SELECT DENSE_RANK() OVER (ORDER BY abstract_id, concrete_id) AS match_id,
+                   abstract_id AS entity_id, 'AbstractClass' AS role
+            FROM base
+            SQL;
+
+    private const SELECT_CONCRETE = <<<'SQL'
+            SELECT DENSE_RANK() OVER (ORDER BY abstract_id, concrete_id) AS match_id,
+                   concrete_id AS entity_id, 'ConcreteClass' AS role
+            FROM base
+            SQL;
+
+    public function candidateSql(): string
+    {
+        return
+            self::CTE_BASE . "\n" .
+            self::SELECT_ABSTRACT . "\n" .
+            "UNION ALL\n" .
+            self::SELECT_CONCRETE;
+    }
+}

tests/Unit/Detector/Pattern/Builtin/BuilderTest.php
@@ -1,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace SineFine\Mnemosyne\Tests\Unit\Detector\Pattern\Builtin;
+
+use PHPUnit\Framework\TestCase;
+use SineFine\Mnemosyne\Detector\Pattern\Catalog\Builder;
+
+final class BuilderTest extends TestCase
+{
+    private Builder $pattern;
+
+    protected function setUp(): void
+    {
+        $this->pattern = new Builder();
+    }
+
+    public function testName(): void
+    {
+        $this->assertSame('builder', $this->pattern->name());
+    }
+
+    public function testRoles(): void
+    {
+        $this->assertSame(['Builder', 'ConcreteBuilder', 'Director'], $this->pattern->roles());
+    }
+
+    public function testCandidateSql(): void
+    {
+        $this->assertNotEmpty($this->pattern->candidateSql());
+    }
+}

tests/Unit/Detector/Pattern/Builtin/StrategyTest.php
@@ -1,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace SineFine\Mnemosyne\Tests\Unit\Detector\Pattern\Builtin;
+
+use PHPUnit\Framework\TestCase;
+use SineFine\Mnemosyne\Detector\Pattern\Catalog\Strategy;
+
+final class StrategyTest extends TestCase
+{
+    private Strategy $pattern;
+
+    protected function setUp(): void
+    {
+        $this->pattern = new Strategy();
+    }
+
+    public function testName(): void
+    {
+        $this->assertSame('strategy', $this->pattern->name());
+    }
+
+    public function testRoles(): void
+    {
+        $this->assertSame(['Strategy', 'ConcreteStrategy', 'Context'], $this->pattern->roles());
+    }
+
+    public function testCandidateSql(): void
+    {
+        $this->assertNotEmpty($this->pattern->candidateSql());
+    }
+}