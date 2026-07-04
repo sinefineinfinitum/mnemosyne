sinefineinfinitum/ponymator

feat: integrate pattern detection into main app

src/Cli/Graph/ClearCommand.php
@@ -3,14 +3,16 @@
 namespace SineFine\Ponymator\Cli\Graph;
 
 use SineFine\Ponymator\Cli\Command;
+use SineFine\Ponymator\Cli\Error\ExitCode;
+use SineFine\Ponymator\Config;
 use SineFine\Ponymator\Db\PDOFactory;
 use SineFine\Ponymator\Graph\Experimental\Schema;
 
 class ClearCommand
 {
-    public function execute(Command $cmd): void
+    public function execute(Command $cmd, Config $config): void
     {
-        $factory = new PDOFactory($cmd);
+        $factory = new PDOFactory($cmd, $config);
         $dbPath = $factory->resolvePath();
         $isNew = !file_exists($dbPath);
         $pdo = $factory->connect();

sinefineinfinitum/mnemosyne

src/Cli/Graph/ClearCommand.php
@@ -3,14 +3,16 @@
 namespace SineFine\Mnemosyne\Cli\Graph;
 
 use SineFine\Mnemosyne\Cli\Command;
+use SineFine\Mnemosyne\Cli\Error\ExitCode;
+use SineFine\Mnemosyne\Config;
 use SineFine\Mnemosyne\Db\PDOFactory;
 use SineFine\Mnemosyne\Graph\Experimental\Schema;
 
 class ClearCommand
 {
-    public function execute(Command $cmd): void
+    public function execute(Command $cmd, Config $config): void
     {
-        $factory = new PDOFactory($cmd);
+        $factory = new PDOFactory($cmd, $config);
         $dbPath = $factory->resolvePath();
         $isNew = !file_exists($dbPath);
         $pdo = $factory->connect();