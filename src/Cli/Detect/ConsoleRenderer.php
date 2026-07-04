sinefineinfinitum/ponymator

feat: add detect CLI command

src/Cli/Detect/ConsoleRenderer.php
@@ -1,0 +1,126 @@
+<?php declare(strict_types=1);
+
+namespace SineFine\Ponymator\Cli\Detect;
+
+final class ConsoleRenderer
+{
+    private const HEADERS = ['Pattern', 'Role', 'Class'];
+    private const COL_WIDTHS = [9, 15, 46]; // Separators width 2+3+3+2, sum of all widths must be = 80
+
+    /**
+     * @param list<list<list<string>>> $blocks
+     */
+    public function render(array $blocks): void
+    {
+        if ($blocks === []) {
+            echo "No pattern matches found.\n";
+            return;
+        }
+
+        $widths = self::COL_WIDTHS;
+
+        $border = '+' . implode('+', array_map(fn(int $w) => str_repeat('-', $w + 2), $widths)) . '+';
+
+        echo $border . "\n";
+        $this->outputRow(self::HEADERS, $widths);
+        echo $border . "\n";
+
+        $last = array_key_last($blocks);
+        foreach ($blocks as $idx => $rows) {
+            foreach ($rows as $row) {
+                $this->outputMultiRow($row, $widths);
+            }
+            if ($idx !== $last) {
+                echo $border . "\n";
+            }
+        }
+
+        echo $border . "\n";
+    }
+
+    /**
+     * @param string[] $cells
+     * @param int[]    $widths
+     */
+    private function outputMultiRow(array $cells, array $widths): void
+    {
+        $wrapped = [];
+        $maxLines = 1;
+        foreach ($cells as $i => $cell) {
+            $separator = match ($i) {
+                0 => '_',
+                2 => '\\',
+                default => '',
+            };
+            $appendSemicolon = $i === 2;
+            $lines = $this->wrapCell($cell, $widths[$i], $separator, $appendSemicolon);
+            $wrapped[] = $lines;
+            $cnt = count($lines);
+            if ($cnt > $maxLines) {
+                $maxLines = $cnt;
+            }
+        }
+
+        for ($line = 0; $line < $maxLines; $line++) {
+            $parts = [];
+            foreach ($wrapped as $i => $lines) {
+                $parts[] = $lines[$line] ?? '';
+            }
+            $this->outputRow($parts, $widths);
+        }
+    }
+
+    /**
+     * @return list<string>
+     */
+    private function wrapCell(string $text, int $width, string $separator = '\\', bool $appendSemicolon = false): array
+    {
+        if ($text === '') {
+            return [''];
+        }
+
+        if ($separator === '' || mb_strlen($text) <= $width) {
+            return [$appendSemicolon ? $text . ';' : $text];
+        }
+
+        $segments = explode($separator, $text);
+
+        if (count($segments) <= 1) {
+            return [$appendSemicolon ? $text . ';' : $text];
+        }
+
+        $lines = [];
+        $current = '';
+
+        foreach ($segments as $seg) {
+            $candidate = $current === '' ? $seg : $current . $separator . $seg;
+            if (mb_strlen($candidate) <= $width) {
+                $current = $candidate;
+            } else {
+                if ($current !== '') {
+                    $lines[] = $current;
+                }
+                $current = ($separator === '_' ? '' : $separator) . $seg;
+            }
+        }
+
+        if ($current !== '') {
+            $lines[] = $appendSemicolon ? $current . ';' : $current;
+        }
+
+        return $lines;
+    }
+
+    /**
+     * @param string[] $cells
+     * @param int[]    $widths
+     */
+    private function outputRow(array $cells, array $widths): void
+    {
+        echo '|';
+        foreach ($cells as $i => $cell) {
+            echo ' ' . str_pad($cell, $widths[$i]) . ' |';
+        }
+        echo "\n";
+    }
+}

src/Cli/Detect/DetectCommand.php
@@ -1,0 +1,52 @@
+<?php declare(strict_types=1);
+
+namespace SineFine\Ponymator\Cli\Detect;
+
+use PDO;
+use SineFine\Ponymator\Cli\Command;
+use SineFine\Ponymator\Detector\Pattern\Catalog\Adapter;
+use SineFine\Ponymator\Detector\Pattern\Catalog\Builder;
+use SineFine\Ponymator\Detector\Pattern\Catalog\Decorator;
+use SineFine\Ponymator\Detector\Pattern\Catalog\FactoryMethod;
+use SineFine\Ponymator\Detector\Pattern\Catalog\Singleton;
+use SineFine\Ponymator\Detector\Pattern\Catalog\Strategy;
+use SineFine\Ponymator\Detector\Pattern\Catalog\TemplateMethod;
+use SineFine\Ponymator\Detector\Pattern\Engine\PatternRegistry;
+use SineFine\Ponymator\Detector\Pattern\Engine\Engine;
+use SineFine\Ponymator\Graph\Experimental\GraphQuery;
+use Throwable;
+
+final class DetectCommand
+{
+    /**
+     * @throws Throwable
+     */
+    public function execute(Command $cmd, GraphQuery $query, ?PDO $readOnlyPdo = null): void
+    {
+        $registry = new PatternRegistry(
+            [
+            new Adapter(),
+            new Builder(),
+            new Decorator(),
+            new FactoryMethod(),
+            new Strategy(),
+            new Singleton(),
+            new TemplateMethod(),
+            ]
+        );
+
+        $pdo = $query->getPdo();
+
+        $engine = new Engine($registry, $pdo, $readOnlyPdo);
+        $result = $engine->run();
+
+        foreach ($result->errors as $error) {
+            fwrite(STDERR, "Warning: $error\n");
+        }
+
+        $view = new PatternView($result, $query);
+
+        $renderer = new ConsoleRenderer();
+        $renderer->render($view->blocks);
+    }
+}

src/Cli/Detect/PatternView.php
@@ -1,0 +1,69 @@
+<?php declare(strict_types=1);
+
+namespace SineFine\Ponymator\Cli\Detect;
+
+use SineFine\Ponymator\Detector\Pattern\Model\PatternResult;
+use SineFine\Ponymator\Graph\Experimental\GraphQuery;
+
+final class PatternView
+{
+    /** @var list<list<list<string>>> */
+    public array $blocks;
+
+    public function __construct(PatternResult $result, GraphQuery $query)
+    {
+        $blocks = [];
+
+        foreach ($result->matches as $match) {
+            $block = [];
+            $hasNaming = false;
+
+            foreach ($match->participants as $i => $participant) {
+                $entity = $query->findEntityById($participant->entityId);
+                $fqn = $entity !== null ? $entity['fqn'] : '#' . $participant->entityId;
+
+                if (!$hasNaming) {
+                    $hasNaming = $this->hasNaming($match->pattern->name(), $fqn, $match->pattern->roles());
+                }
+
+                $block[] = [
+                    $i === 0 ? $match->pattern->name() : '',
+                    $participant->role,
+                    $fqn,
+                ];
+            }
+
+            if ($hasNaming && $block !== []) {
+                array_splice($block, 1, 0, [['+naming', '', '']]);
+            }
+
+            $blocks[] = $block;
+        }
+
+        $this->blocks = $blocks;
+    }
+
+    /**
+     * @param  string   $patternName
+     * @param  string   $fqn
+     * @param  string[] $roles
+     * @return bool
+     */
+    private function hasNaming(string $patternName, string $fqn, array $roles): bool
+    {
+        $parts = explode('\\', $fqn);
+        $shortName = strtolower(end($parts));
+
+        if (str_contains($shortName, strtolower($patternName))) {
+            return true;
+        }
+
+        foreach ($roles as $role) {
+            if (str_contains($shortName, strtolower($role))) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}

tests/Unit/Cli/Detect/ArgumentParserDetectTest.php
@@ -1,0 +1,42 @@
+<?php declare(strict_types=1);
+
+namespace SineFine\Ponymator\Tests\Unit\Cli\Detect;
+
+use PHPUnit\Framework\TestCase;
+use SineFine\Ponymator\Cli\ArgumentParser;
+
+final class ArgumentParserDetectTest extends TestCase
+{
+    public function testParsesDetectCommand(): void
+    {
+        $cmd = ArgumentParser::parse(['ponymator', 'detect']);
+
+        $this->assertSame('detect', $cmd->group);
+        $this->assertNull($cmd->subcommand);
+        $this->assertTrue($cmd->helpRequested);
+    }
+
+    public function testParsesDetectWithDbPath(): void
+    {
+        $cmd = ArgumentParser::parse(['ponymator', 'detect', '--db-path=/tmp/test.db']);
+
+        $this->assertSame('detect', $cmd->group);
+        $this->assertSame('/tmp/test.db', $cmd->dbPath);
+    }
+
+    public function testParsesDetectWithConfigPath(): void
+    {
+        $cmd = ArgumentParser::parse(['ponymator', 'detect', '--config=/path/to/config.json']);
+
+        $this->assertSame('detect', $cmd->group);
+        $this->assertSame('/path/to/config.json', $cmd->configPath);
+    }
+
+    public function testParsesDetectHelp(): void
+    {
+        $cmd = ArgumentParser::parse(['ponymator', 'detect', '--help']);
+
+        $this->assertTrue($cmd->helpRequested);
+    }
+
+}

tests/Unit/Cli/Detect/DetectCommandTest.php
@@ -1,0 +1,87 @@
+<?php declare(strict_types=1);
+
+namespace SineFine\Ponymator\Tests\Unit\Cli\Detect;
+
+use PDO;
+use PHPUnit\Framework\TestCase;
+use SineFine\Ponymator\Cli\Command;
+use SineFine\Ponymator\Cli\Detect\DetectCommand;
+use SineFine\Ponymator\Graph\Experimental\GraphCommand;
+use SineFine\Ponymator\Graph\Experimental\GraphQuery;
+use SineFine\Ponymator\Graph\Experimental\Schema;
+
+final class DetectCommandTest extends TestCase
+{
+    private PDO $pdo;
+    private GraphQuery $query;
+    private GraphCommand $command;
+
+    protected function setUp(): void
+    {
+        $this->pdo = new PDO('sqlite::memory:');
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        Schema::create($this->pdo);
+        $this->command = new GraphCommand($this->pdo);
+        $this->query = new GraphQuery($this->pdo);
+    }
+
+    public function testRenderTableWithResults(): void
+    {
+        $targetId = $this->command->insertEntity(
+            'App\\TargetInterface', 'TargetInterface', 'interface', null, null, null, [],
+        );
+        $adapteeId = $this->command->insertEntity(
+            'App\\LegacyService', 'LegacyService', 'class', null, null, null, [],
+        );
+        $adapterId = $this->command->insertEntity(
+            'App\\ConcreteAdapter', 'ConcreteAdapter', 'class', null, null, null, [],
+        );
+        $this->command->insertRelationship($adapterId, $targetId, null, 'implements', null);
+        $this->command->insertRelationship($adapterId, $adapteeId, null, 'dependency', null);
+
+        $cmd = new Command(
+            group: 'detect',
+            subcommand: null,
+            positionalArgs: [],
+            configPath: null,
+            output: 'md',
+            dbPath: null,
+            depth: null,
+            helpRequested: false,
+        );
+
+        $detectCommand = new DetectCommand();
+
+        ob_start();
+        $detectCommand->execute($cmd, $this->query);
+        $output = ob_get_clean();
+
+        $this->assertStringContainsString('Pattern', $output);
+        $this->assertStringContainsString('Role', $output);
+        $this->assertStringContainsString('Class', $output);
+        $this->assertStringContainsString('adapter', $output);
+    }
+
+    public function testRenderTableNoMatches(): void
+    {
+        $cmd = new Command(
+            group: 'detect',
+            subcommand: null,
+            positionalArgs: [],
+            configPath: null,
+            output: 'md',
+            dbPath: null,
+            depth: null,
+            helpRequested: false,
+        );
+
+        $detectCommand = new DetectCommand();
+
+        ob_start();
+        $detectCommand->execute($cmd, $this->query);
+        $output = ob_get_clean();
+
+        $this->assertStringContainsString('No pattern matches found.', $output);
+    }
+
+}

tests/Unit/Cli/Detect/HelpPrinterDetectTest.php
@@ -1,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace SineFine\Ponymator\Tests\Unit\Cli\Detect;
+
+use PHPUnit\Framework\TestCase;
+use SineFine\Ponymator\Cli\HelpPrinter;
+
+final class HelpPrinterDetectTest extends TestCase
+{
+    public function testPrintDetectHelp(): void
+    {
+        ob_start();
+        HelpPrinter::printDetectHelp();
+        $output = ob_get_clean();
+
+        $this->assertStringContainsString('ponymator detect', $output);
+        $this->assertStringContainsString('--db-path', $output);
+        $this->assertStringContainsString('--config', $output);
+        $this->assertStringContainsString('--help', $output);
+    }
+
+    public function testMainHelpIncludesDetect(): void
+    {
+        ob_start();
+        HelpPrinter::printHelp();
+        $output = ob_get_clean();
+
+        $this->assertStringContainsString('detect', $output);
+        $this->assertStringContainsString('Detect design patterns', $output);
+    }
+}

sinefineinfinitum/mnemosyne

src/Cli/Detect/ConsoleRenderer.php
@@ -1,0 +1,126 @@
+<?php declare(strict_types=1);
+
+namespace SineFine\Mnemosyne\Cli\Detect;
+
+final class ConsoleRenderer
+{
+    private const HEADERS = ['Pattern', 'Role', 'Class'];
+    private const COL_WIDTHS = [9, 15, 46]; // Separators width 2+3+3+2, sum of all widths must be = 80
+
+    /**
+     * @param list<list<list<string>>> $blocks
+     */
+    public function render(array $blocks): void
+    {
+        if ($blocks === []) {
+            echo "No pattern matches found.\n";
+            return;
+        }
+
+        $widths = self::COL_WIDTHS;
+
+        $border = '+' . implode('+', array_map(fn(int $w) => str_repeat('-', $w + 2), $widths)) . '+';
+
+        echo $border . "\n";
+        $this->outputRow(self::HEADERS, $widths);
+        echo $border . "\n";
+
+        $last = array_key_last($blocks);
+        foreach ($blocks as $idx => $rows) {
+            foreach ($rows as $row) {
+                $this->outputMultiRow($row, $widths);
+            }
+            if ($idx !== $last) {
+                echo $border . "\n";
+            }
+        }
+
+        echo $border . "\n";
+    }
+
+    /**
+     * @param string[] $cells
+     * @param int[]    $widths
+     */
+    private function outputMultiRow(array $cells, array $widths): void
+    {
+        $wrapped = [];
+        $maxLines = 1;
+        foreach ($cells as $i => $cell) {
+            $separator = match ($i) {
+                0 => '_',
+                2 => '\\',
+                default => '',
+            };
+            $appendSemicolon = $i === 2;
+            $lines = $this->wrapCell($cell, $widths[$i], $separator, $appendSemicolon);
+            $wrapped[] = $lines;
+            $cnt = count($lines);
+            if ($cnt > $maxLines) {
+                $maxLines = $cnt;
+            }
+        }
+
+        for ($line = 0; $line < $maxLines; $line++) {
+            $parts = [];
+            foreach ($wrapped as $i => $lines) {
+                $parts[] = $lines[$line] ?? '';
+            }
+            $this->outputRow($parts, $widths);
+        }
+    }
+
+    /**
+     * @return list<string>
+     */
+    private function wrapCell(string $text, int $width, string $separator = '\\', bool $appendSemicolon = false): array
+    {
+        if ($text === '') {
+            return [''];
+        }
+
+        if ($separator === '' || mb_strlen($text) <= $width) {
+            return [$appendSemicolon ? $text . ';' : $text];
+        }
+
+        $segments = explode($separator, $text);
+
+        if (count($segments) <= 1) {
+            return [$appendSemicolon ? $text . ';' : $text];
+        }
+
+        $lines = [];
+        $current = '';
+
+        foreach ($segments as $seg) {
+            $candidate = $current === '' ? $seg : $current . $separator . $seg;
+            if (mb_strlen($candidate) <= $width) {
+                $current = $candidate;
+            } else {
+                if ($current !== '') {
+                    $lines[] = $current;
+                }
+                $current = ($separator === '_' ? '' : $separator) . $seg;
+            }
+        }
+
+        if ($current !== '') {
+            $lines[] = $appendSemicolon ? $current . ';' : $current;
+        }
+
+        return $lines;
+    }
+
+    /**
+     * @param string[] $cells
+     * @param int[]    $widths
+     */
+    private function outputRow(array $cells, array $widths): void
+    {
+        echo '|';
+        foreach ($cells as $i => $cell) {
+            echo ' ' . str_pad($cell, $widths[$i]) . ' |';
+        }
+        echo "\n";
+    }
+}

src/Cli/Detect/DetectCommand.php
@@ -1,0 +1,52 @@
+<?php declare(strict_types=1);
+
+namespace SineFine\Mnemosyne\Cli\Detect;
+
+use PDO;
+use SineFine\Mnemosyne\Cli\Command;
+use SineFine\Mnemosyne\Detector\Pattern\Catalog\Adapter;
+use SineFine\Mnemosyne\Detector\Pattern\Catalog\Builder;
+use SineFine\Mnemosyne\Detector\Pattern\Catalog\Decorator;
+use SineFine\Mnemosyne\Detector\Pattern\Catalog\FactoryMethod;
+use SineFine\Mnemosyne\Detector\Pattern\Catalog\Singleton;
+use SineFine\Mnemosyne\Detector\Pattern\Catalog\Strategy;
+use SineFine\Mnemosyne\Detector\Pattern\Catalog\TemplateMethod;
+use SineFine\Mnemosyne\Detector\Pattern\Engine\PatternRegistry;
+use SineFine\Mnemosyne\Detector\Pattern\Engine\Engine;
+use SineFine\Mnemosyne\Graph\Experimental\GraphQuery;
+use Throwable;
+
+final class DetectCommand
+{
+    /**
+     * @throws Throwable
+     */
+    public function execute(Command $cmd, GraphQuery $query, ?PDO $readOnlyPdo = null): void
+    {
+        $registry = new PatternRegistry(
+            [
+            new Adapter(),
+            new Builder(),
+            new Decorator(),
+            new FactoryMethod(),
+            new Strategy(),
+            new Singleton(),
+            new TemplateMethod(),
+            ]
+        );
+
+        $pdo = $query->getPdo();
+
+        $engine = new Engine($registry, $pdo, $readOnlyPdo);
+        $result = $engine->run();
+
+        foreach ($result->errors as $error) {
+            fwrite(STDERR, "Warning: $error\n");
+        }
+
+        $view = new PatternView($result, $query);
+
+        $renderer = new ConsoleRenderer();
+        $renderer->render($view->blocks);
+    }
+}

src/Cli/Detect/PatternView.php
@@ -1,0 +1,69 @@
+<?php declare(strict_types=1);
+
+namespace SineFine\Mnemosyne\Cli\Detect;
+
+use SineFine\Mnemosyne\Detector\Pattern\Model\PatternResult;
+use SineFine\Mnemosyne\Graph\Experimental\GraphQuery;
+
+final class PatternView
+{
+    /** @var list<list<list<string>>> */
+    public array $blocks;
+
+    public function __construct(PatternResult $result, GraphQuery $query)
+    {
+        $blocks = [];
+
+        foreach ($result->matches as $match) {
+            $block = [];
+            $hasNaming = false;
+
+            foreach ($match->participants as $i => $participant) {
+                $entity = $query->findEntityById($participant->entityId);
+                $fqn = $entity !== null ? $entity['fqn'] : '#' . $participant->entityId;
+
+                if (!$hasNaming) {
+                    $hasNaming = $this->hasNaming($match->pattern->name(), $fqn, $match->pattern->roles());
+                }
+
+                $block[] = [
+                    $i === 0 ? $match->pattern->name() : '',
+                    $participant->role,
+                    $fqn,
+                ];
+            }
+
+            if ($hasNaming && $block !== []) {
+                array_splice($block, 1, 0, [['+naming', '', '']]);
+            }
+
+            $blocks[] = $block;
+        }
+
+        $this->blocks = $blocks;
+    }
+
+    /**
+     * @param  string   $patternName
+     * @param  string   $fqn
+     * @param  string[] $roles
+     * @return bool
+     */
+    private function hasNaming(string $patternName, string $fqn, array $roles): bool
+    {
+        $parts = explode('\\', $fqn);
+        $shortName = strtolower(end($parts));
+
+        if (str_contains($shortName, strtolower($patternName))) {
+            return true;
+        }
+
+        foreach ($roles as $role) {
+            if (str_contains($shortName, strtolower($role))) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}

tests/Unit/Cli/Detect/ArgumentParserDetectTest.php
@@ -1,0 +1,42 @@
+<?php declare(strict_types=1);
+
+namespace SineFine\Mnemosyne\Tests\Unit\Cli\Detect;
+
+use PHPUnit\Framework\TestCase;
+use SineFine\Mnemosyne\Cli\ArgumentParser;
+
+final class ArgumentParserDetectTest extends TestCase
+{
+    public function testParsesDetectCommand(): void
+    {
+        $cmd = ArgumentParser::parse(['ponymator', 'detect']);
+
+        $this->assertSame('detect', $cmd->group);
+        $this->assertNull($cmd->subcommand);
+        $this->assertTrue($cmd->helpRequested);
+    }
+
+    public function testParsesDetectWithDbPath(): void
+    {
+        $cmd = ArgumentParser::parse(['ponymator', 'detect', '--db-path=/tmp/test.db']);
+
+        $this->assertSame('detect', $cmd->group);
+        $this->assertSame('/tmp/test.db', $cmd->dbPath);
+    }
+
+    public function testParsesDetectWithConfigPath(): void
+    {
+        $cmd = ArgumentParser::parse(['ponymator', 'detect', '--config=/path/to/config.json']);
+
+        $this->assertSame('detect', $cmd->group);
+        $this->assertSame('/path/to/config.json', $cmd->configPath);
+    }
+
+    public function testParsesDetectHelp(): void
+    {
+        $cmd = ArgumentParser::parse(['ponymator', 'detect', '--help']);
+
+        $this->assertTrue($cmd->helpRequested);
+    }
+
+}

tests/Unit/Cli/Detect/DetectCommandTest.php
@@ -1,0 +1,87 @@
+<?php declare(strict_types=1);
+
+namespace SineFine\Mnemosyne\Tests\Unit\Cli\Detect;
+
+use PDO;
+use PHPUnit\Framework\TestCase;
+use SineFine\Mnemosyne\Cli\Command;
+use SineFine\Mnemosyne\Cli\Detect\DetectCommand;
+use SineFine\Mnemosyne\Graph\Experimental\GraphCommand;
+use SineFine\Mnemosyne\Graph\Experimental\GraphQuery;
+use SineFine\Mnemosyne\Graph\Experimental\Schema;
+
+final class DetectCommandTest extends TestCase
+{
+    private PDO $pdo;
+    private GraphQuery $query;
+    private GraphCommand $command;
+
+    protected function setUp(): void
+    {
+        $this->pdo = new PDO('sqlite::memory:');
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        Schema::create($this->pdo);
+        $this->command = new GraphCommand($this->pdo);
+        $this->query = new GraphQuery($this->pdo);
+    }
+
+    public function testRenderTableWithResults(): void
+    {
+        $targetId = $this->command->insertEntity(
+            'App\\TargetInterface', 'TargetInterface', 'interface', null, null, null, [],
+        );
+        $adapteeId = $this->command->insertEntity(
+            'App\\LegacyService', 'LegacyService', 'class', null, null, null, [],
+        );
+        $adapterId = $this->command->insertEntity(
+            'App\\ConcreteAdapter', 'ConcreteAdapter', 'class', null, null, null, [],
+        );
+        $this->command->insertRelationship($adapterId, $targetId, null, 'implements', null);
+        $this->command->insertRelationship($adapterId, $adapteeId, null, 'dependency', null);
+
+        $cmd = new Command(
+            group: 'detect',
+            subcommand: null,
+            positionalArgs: [],
+            configPath: null,
+            output: 'md',
+            dbPath: null,
+            depth: null,
+            helpRequested: false,
+        );
+
+        $detectCommand = new DetectCommand();
+
+        ob_start();
+        $detectCommand->execute($cmd, $this->query);
+        $output = ob_get_clean();
+
+        $this->assertStringContainsString('Pattern', $output);
+        $this->assertStringContainsString('Role', $output);
+        $this->assertStringContainsString('Class', $output);
+        $this->assertStringContainsString('adapter', $output);
+    }
+
+    public function testRenderTableNoMatches(): void
+    {
+        $cmd = new Command(
+            group: 'detect',
+            subcommand: null,
+            positionalArgs: [],
+            configPath: null,
+            output: 'md',
+            dbPath: null,
+            depth: null,
+            helpRequested: false,
+        );
+
+        $detectCommand = new DetectCommand();
+
+        ob_start();
+        $detectCommand->execute($cmd, $this->query);
+        $output = ob_get_clean();
+
+        $this->assertStringContainsString('No pattern matches found.', $output);
+    }
+
+}

tests/Unit/Cli/Detect/HelpPrinterDetectTest.php
@@ -1,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace SineFine\Mnemosyne\Tests\Unit\Cli\Detect;
+
+use PHPUnit\Framework\TestCase;
+use SineFine\Mnemosyne\Cli\HelpPrinter;
+
+final class HelpPrinterDetectTest extends TestCase
+{
+    public function testPrintDetectHelp(): void
+    {
+        ob_start();
+        HelpPrinter::printDetectHelp();
+        $output = ob_get_clean();
+
+        $this->assertStringContainsString('ponymator detect', $output);
+        $this->assertStringContainsString('--db-path', $output);
+        $this->assertStringContainsString('--config', $output);
+        $this->assertStringContainsString('--help', $output);
+    }
+
+    public function testMainHelpIncludesDetect(): void
+    {
+        ob_start();
+        HelpPrinter::printHelp();
+        $output = ob_get_clean();
+
+        $this->assertStringContainsString('detect', $output);
+        $this->assertStringContainsString('Detect design patterns', $output);
+    }
+}